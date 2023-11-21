c20426e0c80164dc1a3130a5b8be22bbccc276da
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-11-21 10:09:06 +0100

    add °C

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ search.addEventListener('click', () => {
                 image.src = '';
             
         }
-        temperature.innerHTML = `${parseInt(json.main.temp)}<span>C</span>`;
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
 
         description.innerHTML = `${json.weather[0].description}`;
         humidity.innerHTML = `${json.main.humidity}%`;
